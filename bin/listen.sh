88b1817b083bdf6beafedab34a34dbb640dd6500
Antonio Tapiador del Dujo <atd@singularities.org>
2020-01-31 21:49:04 +0100

    Implement listener using Gio.Subprocess

--- bin/listen.sh
+++ bin/listen.sh
@@ -1,1 +1,3 @@
-/usr/bin/arecord -D plughw:1,0 -d 5 2>/dev/null | /usr/bin/sox -t .wav - -n stat 2>&1 | grep "Maximum amplitude" | cut -f 2 -d:
+while true; do
+  /usr/bin/arecord -D plughw:1,0 -d 5 2>/dev/null | /usr/bin/sox -t .wav - -n stat 2>&1 | grep "Maximum amplitude" | cut -f 2 -d:
+done
